7c927ac304241e11131c4de3e5a82de7806d53b2
Marvyn MAQUIGNON <mamaquig@e1r3p20.42.fr>
2019-10-08 17:22:31 +0200

    d2 ajout exos

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,21 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   atoi.c                                             :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mamaquig <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 14:44:18 by mamaquig          #+#    #+#             */
-/*   Updated: 2019/10/07 15:37:49 by mamaquig         ###   ########.fr       */
+/*   Updated: 2019/10/08 17:21:47 by mamaquig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-
-int		ft_atoi(char *str)
+int		ft_atoi(const char *str)
 {
-}
+	int i;
+	int sig;
+	int res;
 
-int		main(void)
-{
+	i = 0;
+	sig = 1;
+	res = 0;
+
+	while (str[i] == ' ' || str[i] == '\f' || str[i] == '\t' || str[i] == '\n'
+		   	|| str[i] == '\r' || str[i] == '\v')
+		i++;
+
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sig = -1;
+		i++;
+	}
+
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = (res * 10) + (str[i] - 48);
+		i++;
+	}
+
+	return (res * sig);
 }
