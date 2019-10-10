0aefa1b260018f7fd8eb129288260b95ff5bc458
Marvyn MAQUIGNON <mamaquig@e1r4p22.42.fr>
2019-10-10 11:47:39 +0200

    d4 strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamaquig <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/09 18:39:13 by mamaquig          #+#    #+#             */
+/*   Updated: 2019/10/10 11:45:48 by mamaquig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	int i;
+	int j;
+
+	i = 0;
+	if (needle[0] == 0)
+		return ((char *)haystack);
+	while (haystack[i] && i < len)
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j])
+		{
+			if (needle[j + 1] == 0)
+				return ((char *)&haystack[i]);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
