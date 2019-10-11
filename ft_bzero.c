76cd99f4ca3a6f99b9b8c2d134da07ffb4865e4f
Marvyn MAQUIGNON <mamaquig@e1r4p21.42.fr>
2019-10-11 16:21:47 +0200

    d5 bzero modified

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,20 +6,13 @@
 /*   By: mamaquig <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 17:39:08 by mamaquig          #+#    #+#             */
-/*   Updated: 2019/10/11 15:50:45 by mamaquig         ###   ########.fr       */
+/*   Updated: 2019/10/11 16:21:12 by mamaquig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include <libft.h>
 
 void	ft_bzero(void *s, size_t n)
 {
-	size_t i;
-
-	i = 0;
-	while (i < n)
-	{
-		((unsigned char *)s)[i] = 0;
-		i++;
-	}
+	memset(s, 0, n);
 }
