eaa7913e14c4df1d70898e0195e8ae1344622f63
Marvyn MAQUIGNON <mamaquig@e1r4p17.42.fr>
2019-10-11 18:28:48 +0200

    d5 strdup added

---
+++ ft_strdup.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamaquig <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/11 18:22:06 by mamaquig          #+#    #+#             */
+/*   Updated: 2019/10/11 18:28:23 by mamaquig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+char	*ft_strdup(const char *s1)
+{
+	int		i;
+	int		s;
+	char	*dest;
+
+	i = 0;
+	s = ft_strlen(s1);
+	if (!(dest = malloc(sizeof(*dest) * s)))
+		return (NULL);
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
