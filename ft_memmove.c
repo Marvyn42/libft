107d8dc7af9d5f3133b6b0b1a90292fc64de428a
Marvyn MAQUIGNON <mamaquig@e3r8p14.42.fr>
2019-10-14 13:17:47 +0200

    d6 opti memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mamaquig <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/10 14:07:21 by mamaquig          #+#    #+#             */
-/*   Updated: 2019/10/11 18:41:56 by mamaquig         ###   ########.fr       */
+/*   Updated: 2019/10/14 13:16:51 by mamaquig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,9 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	char *temp;
 	size_t i;
 
-	temp = (char *)dst;
+	temp = (char *)src;
 	i = 0;
 	while (i < len)
-	{
-		temp[i] = ((char *)src)[i];
-		i++;
-	}
-	i = 0;
-	while (temp[i])
 	{
 		((char *)dst)[i] = temp[i];
 		i++;
