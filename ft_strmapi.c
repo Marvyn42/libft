6c552380eb92ccae71d1199bf3f098960941e871
Marvyn MAQUIGNON <mamaquig@e1r11p17.42.fr>
2019-10-21 16:38:30 +0200

    strmapi modified, libft added

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,13 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strmapi.c                                          :+:      :+:    :+:   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mamaquig <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 14:06:19 by mamaquig          #+#    #+#             */
-/*   Updated: 2019/10/21 14:06:38 by mamaquig         ###   ########.fr       */
+/*   Updated: 2019/10/21 16:37:56 by mamaquig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*str;
+
+	i = 0;
+	str = malloc(sizeof(char) * ft_strlen(s));
+	while (s[i])
+	{
+		str[i] = (*f)(i, s[i]);
+		i++;
+	}
+	return (str);
+}	
