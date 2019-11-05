1604e07c2ae8fc76a073b396037fdec37c7e6e03
Marvyn MAQUIGNON <mamaquig@e1r8p2.42.fr>
2019-11-05 13:50:02 +0100

    libft.h finished and normed

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mamaquig <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 15:54:00 by mamaquig          #+#    #+#             */
-/*   Updated: 2019/11/04 11:17:30 by mamaquig         ###   ########.fr       */
+/*   Updated: 2019/11/05 13:49:36 by mamaquig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,5 +58,14 @@ char				*ft_substr(char const *s, unsigned int start, size_t len);
 int					ft_tolower(int c);
 int					ft_toupper(int c);
 t_list				*ft_lstnew(void *content);
+void				ft_lstadd_front(t_list **alst, t_list *new);
+int					ft_lstsize(t_list *lst);
+t_list				*ft_lstlast(t_list *lst);
+void				ft_lstadd_back(t_list **alst, t_list *new);
+void				ft_lstdelone(t_list *lst, void (*del)(void*));
+void				ft_lstclear(t_list **lst, void (*del)(void*));
+void				ft_lstiter(t_list *lst, void (*f)(void *));
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *), \
+								void (*del)(void *));
 
 #endif
